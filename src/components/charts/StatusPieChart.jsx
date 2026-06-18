31b4aaa21b5675648b02960b76f99ea7dcbf54e1
Maria Avdeeva <maryavdeeva30012006@gmail.com>
2026-06-19 02:17:33 +0500

    Уменьшение круговой диаграммы

--- src/components/charts/StatusPieChart.jsx
+++ src/components/charts/StatusPieChart.jsx
@@ -3,17 +3,10 @@ import { ChartCard } from "./ChartCard.jsx";
 
 const COLORS = ["#A5C4FD", "#E3B0FF", "#FF7676"];
 
-const renderLabel = ({
-  cx,
-  cy,
-  midAngle,
-  outerRadius,
-  name,
-  percentage,
-}) => {
+const renderLabel = ({ cx, cy, midAngle, outerRadius, name, percentage }) => {
   const RADIAN = Math.PI / 180;
 
-  const radius = outerRadius + 30; // расстояние от диаграммы
+  const radius = outerRadius + 30;
 
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
@@ -25,9 +18,13 @@ const renderLabel = ({
       fill="#000"
       fontSize={14}
       textAnchor={x > cx ? "start" : "end"}
-      dominantBaseline="central"
     >
-      {`${name} (${percentage}%)`}
+      <tspan x={x} dy="0">
+        {name}
+      </tspan>
+      <tspan x={x} dy="16">
+        {`(${percentage}%)`}
+      </tspan>
     </text>
   );
 };
@@ -48,18 +45,15 @@ export function StatusPieChart({
             nameKey="name"
             cx="50%"
             cy="50%"
-            innerRadius={81.5}
-            outerRadius={139.5}
+            innerRadius={70}
+            outerRadius={120}
             cornerRadius={8}
             paddingAngle={2}
             label={renderLabel}
             labelLine={false}
           >
             {data.map((_, index) => (
-              <Cell
-                key={index}
-                fill={colors[index % colors.length]}
-              />
+              <Cell key={index} fill={colors[index % colors.length]} />
             ))}
           </Pie>
         </PieChart>
